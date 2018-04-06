f037b2a7fc2548d6a9d0fa45a1f6a6651eabcb36
Samu27 <samuele.mori.96@gmail.com>
2018-04-06 17:15:19 +0200

    bash launcher

--- script/launcher.sh
+++ script/launcher.sh
@@ -10,7 +10,5 @@ sleep 2s
 xterm -hold -e "rosrun homework1_ros talker.py" &
 # Avvio nodo filter
 xterm -hold -e "rosrun homework1_ros filter.py" & 
-#xterm -hold -e bash secondo_nodo.sh &
 # Avvio nodo listener
 xterm -hold -e "rosrun homework1_ros listener.py" &
-#xterm -hold -e bash terzo_nodo.sh &
