e34f48ab8db889a61070e06846add716763a9b9d
Samu27 <samuele.mori.96@gmail.com>
2018-04-13 08:33:02 +0200

    node filter

--- src/filter.py
+++ src/filter.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python2
 #coding=utf-8
 
+'''
+Nodo filter.
+Invia sul topic "filter" la modalita' di stampa scelta dall'utente.
+'''
+
 import rospy
 from std_msgs.msg import String
 import sys
 import signal
 
 
-def signal_term_handler(signal, frame):
-  '''Handles KeyboardInterrupts to ensure smooth exit'''
-  print("\n")
+def signal_term_handler(signum, frame):
+  ''' Gestisce gli interrupt da tastiera '''
+  print('\n')
   rospy.logerr('User Keyboard interrupt')
-  sys.exit(0)
+  sys.exit(128+signum)
+
 
 def filter():
 	pub = rospy.Publisher('filter', String, queue_size=10)
 	rospy.init_node('Filter', anonymous=True)
 
 	while True:
+		# Menu
 		choice = raw_input('''\nDigitare:
-\t- 'a' verrà stampato tutto il messaggio
-\t- 'n' mostrerà solo il nome
-\t- 'e' mostrerà solo l’età
-\t- 'c' mostrerà solo il corso di laurea
-\t- '0' terminerà filter MA non talker e listener.
+\t- 'a' per stampare tutto il messaggio
+\t- 'n' per stampare solo il nome
+\t- 'e' per stampare solo l’eta'
+\t- 'c' per stampare solo il corso di laurea
+\t- '0' per terminare il nodo filter.
 >>> ''')
-		if choice in ("e", "n", "a", "c"):
+
+		if choice in ("a", "n", "e", "c"):
 			rospy.loginfo(choice)
 			pub.publish(choice)
 		if choice == "0":
@@ -33,6 +41,7 @@ def filter():
 
 
 if __name__ == '__main__':
+	# Settaggio handler dei segnali
 	signal.signal(signal.SIGINT, signal_term_handler)
 	signal.signal(signal.SIGQUIT, signal_term_handler)
 	signal.signal(signal.SIGTSTP, signal_term_handler)
